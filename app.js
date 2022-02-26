891b133effac9787eb2a6e2841da5985e92c29ac
aitldev14 <aitldev14@gmail.com>
2022-02-26 16:33:58 +0600

    26/02/22

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ function first_image(){
   document.getElementById("openion-2").innerHTML =  document.getElementById("openion-1").innerText;
   document.getElementById("image-1").src = "images/3.png";
   document.getElementById("1").style.cssText = `border-radius: 50%; border: 2px dashed #F27321; padding: 5px;`;
-  document.getElementById("2").style.cssText = `opacity: 0.5; height: 65px; width: 65px;`;
-  document.getElementById("3").style.cssText = `opacity: 0.5; height: 65px; width: 65px;`;
+  document.getElementById("2").style.cssText = `opacity: 0.5; height: 65px; width: 65px; border-radius: 50%;`;
+  document.getElementById("3").style.cssText = `opacity: 0.5; height: 65px; width: 65px; border-radius: 50%;`;
   document.getElementById("name-2").innerHTML = document.getElementById("name-1").innerText;
   document.getElementById("identity-2").innerHTML = document.getElementById("identity-1").innerText; 
    document.getElementById("1").classList.add("active");
@@ -18,8 +18,8 @@ function second_image(){
   document.getElementById("openion-2").innerHTML = "মজারুর কার্যক্রম আমি দেখেছি। তাদের বেশ কিছু ভিডিও দেখার সৌভাগ্য আমার হয়েছে। আমার কাছে মনে হয় এটি যথেষ্ট সম্ভাবনাময় এবং আগামী দিনের জন্য অত্যাবশ্যকীয়। আমি মজারুর উত্তরোত্তর সফলতা কামনা করি।মজারুর কার্যক্রম আমি দেখেছি। তাদের বেশ কিছু ভিডিও দেখার সৌভাগ্য আমার হয়েছে। আমার কাছে মনে হয় এটি যথেষ্ট সম্ভাবনাময় এবং আগামী দিনের জন্য অত্যাবশ্যকীয়। আমি মজারুর উত্তরোত্তর সফলতা কামনা করি।মজারুর কার্যক্রম আমি দেখেছি।"
   document.getElementById("image-1").src = "images/1.png";
   document.getElementById("2").style.cssText = `border-radius: 50%; border: 2px dashed #F27321; padding: 5px;`;
-  document.getElementById("1").style.cssText = `opacity: 0.5; height: 65px; width: 65px;`;
-  document.getElementById("3").style.cssText = 'opacity: 0.5; height: 65px; width: 65px;';
+  document.getElementById("1").style.cssText = `opacity: 0.5; height: 65px; width: 65px; border-radius: 50%;`;
+  document.getElementById("3").style.cssText = 'opacity: 0.5; height: 65px; width: 65px; border-radius: 50%;';
   document.getElementById("name-2").innerHTML = "অধ্যাপক ড. স্বদেশ চন্দ্র সামন্ত";
   document.getElementById("identity-2").innerHTML = "উপাচার্য, পটুয়াখালী বিজ্ঞান ও প্রযুক্তি বিশ্ববিদ্যালয়"; 
   document.getElementById("2").classList.add("active");
@@ -31,8 +31,8 @@ function third_image(){
   document.getElementById("openion-2").innerHTML = document.getElementById("openion-3").innerText;
   document.getElementById("image-1").src = "images/2.png";
   document.getElementById("3").style.cssText = `border-radius: 50%; border: 2px dashed #F27321; padding: 5px;`;
-  document.getElementById("1").style.cssText = `opacity: 0.5; height: 65px; width: 65px;`;
-  document.getElementById("2").style.cssText = `opacity: 0.5; height: 65px; width: 65px;`;
+  document.getElementById("1").style.cssText = `opacity: 0.5; height: 65px; width: 65px; border-radius: 50%;`;
+  document.getElementById("2").style.cssText = `opacity: 0.5; height: 65px; width: 65px; border-radius: 50%;`;
   document.getElementById("name-2").innerHTML = document.getElementById("name-3").innerText;
   document.getElementById("identity-2").innerHTML = document.getElementById("identity-3").innerText;
   document.getElementById("3").classList.add("active"); 
